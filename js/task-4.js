c6890cfcc59babc511ba8b952e98de2f56fde36f
Sashkoe <revolver.bmx@gmail.com>
2024-04-26 22:48:40 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
-
+const LoginForm = document.querySelector(".login-form");
+function funEvent(event) {
+    event.preventDefault();
+    const formElem = event.currentTarget.elements;
+    const formObj = {
+        email: formElem.email.value.trim(),
+        password: formElem.password.value.trim(),
+    };
+    if(formObj.email && formObj.password) {
+        console.log(formObj);
+        LoginForm.reset();
+    }
+    else {
+        alert("All form fields must be filled in");
+    }
+}
+const passwordInput = LoginForm.elements[1];
+function placeholderActive() {
+passwordInput.setAttribute('placeholder', 'type area');
+}
+function placeholderOff(){
+    passwordInput.setAttribute('placeholder', '');
+}
+LoginForm.addEventListener("sumbit", "funEvent");
+passwordInput.addEventListener("click", "placeholderActive");
+passwordInput.addEventListener("blue"," placeholderOff");
