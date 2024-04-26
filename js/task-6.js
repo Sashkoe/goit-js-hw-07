77141563f51934920877f1a463e6482be2c1f682
Sashkoe <revolver.bmx@gmail.com>
2024-04-26 22:48:35 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const boxContainer = document.querySelector(`#boxes`);
+const createButton = document.querySelector(`[data-create]`);
+const destroyButton = document.querySelector(`data-destroy`);
+const input = document.querySelector(`input[type="number"]`);
+function createBoxes(){
+  const amount = Number(input.value);
+  const fragment = document.createDocumentFragment();
+  boxContainer.innerHTML = '';
+  if(amount >= 1 && amount <= 100) {
+for ( let i = 0; i < amount; i++) {
+  const box = document.createElement('div');
+  const size = 30 + i * 10;
+  box.style.width = `${size}px`;
+  box.style.height = `${size}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  fragment.appendChild(box);
+}
+boxContainer.appendChild(fragment);
+input.value = '';
+  }
+}
+function destroyBoxes(){
+  boxContainer.innerHTML = '';
+}
+createButton.addEventListener(`click`, createBoxes);
+destroyButton.addEventListener(`click`, destroyBoxes);
