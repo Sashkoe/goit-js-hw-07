76f4431dbf1117d810f4308975607592ca94957b
Sashkoe <revolver.bmx@gmail.com>
2024-04-28 11:20:20 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,12 @@
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
+const input = document.querySelector("input#name-input");
+const spanName = document.querySelector("span#name-output");
 
-function eventFrom() {
-    const nameInputValue = nameInput.value.trim();
+const inputHandler = (event) => {
+  if (event.target.value.trim() !== "") {
+    spanName.textContent = event.target.value.trim();
+  } else {
+    spanName.textContent = "Anonymous";
+  }
+};
 
-    if(nameInput === "") {
-        nameOutput.textContent = "Anonymous";
-    }
-    else  {
-    nameOutput.textContent = nameInputValue;
-    } 
-}
-nameInput.addEventListener("input", eventFrom);
+input.addEventListener("input", inputHandler);
