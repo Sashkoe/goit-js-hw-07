9cf44155b6a48d05bb456401a5e404d602c7f964
Sashkoe <revolver.bmx@gmail.com>
2024-04-28 11:20:16 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,26 +1,21 @@
-const LoginForm = document.querySelector(".login-form");
-function funEvent(event) {
-    event.preventDefault();
-    const formElem = event.currentTarget.elements;
-    const formObj = {
-        email: formElem.email.value.trim(),
-        password: formElem.password.value.trim(),
+const form = document.querySelector(".login-form");
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  const elements = event.currentTarget.elements;
+  if (
+    elements.email.value.trim() === "" ||
+    elements.password.value.trim() === ""
+  ) {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = {
+      email: elements.email.value.trim(),
+      password: elements.password.value.trim(),
     };
-    if(formObj.email && formObj.password) {
-        console.log(formObj);
-        LoginForm.reset();
-    }
-    else {
-        alert("All form fields must be filled in");
-    }
-}
-const passwordInput = LoginForm.elements[1];
-function placeholderActive() {
-passwordInput.setAttribute('placeholder', 'type area');
-}
-function placeholderOff(){
-    passwordInput.setAttribute('placeholder', '');
-}
-LoginForm.addEventListener("submit", funEvent);;
-passwordInput.addEventListener("click", "placeholderActive");
-passwordInput.addEventListener("blur"," placeholderOff");
+    console.log(formData);
+  }
+  form.reset();
+};
+
+form.addEventListener("submit", submitHandler);
